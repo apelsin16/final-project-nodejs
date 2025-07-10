925d233f3987ea084470587e87a1f65010d86e3f
Mariia <shymanskam524@gmail.com>
2025-07-10 20:02:57 +0200

    add getOwnRecipes and deleteOwnRecipe and  addToFavorites in recipeServices

--- routes/recipesRouter.js
+++ routes/recipesRouter.js
@@ -1,31 +1,41 @@
-import express from 'express';
-import recipesController from '../controllers/recipesControllers.js';
-import ctrlWrapper from '../helpers/controllerWrapper.js';
-import { auth } from '../middlewares/auth.js';
-import { validateQuery, validateParams } from '../middlewares/validation.js';
-import { getFavoritesQuerySchema, recipeIdParamsSchema } from '../schemas/recipesSchemas.js';
+import express from "express";
+import recipesController from "../controllers/recipesControllers.js";
+import ctrlWrapper from "../helpers/controllerWrapper.js";
+import { auth } from "../middlewares/auth.js";
+import { validateQuery, validateParams } from "../middlewares/validation.js";
+import {
+  getFavoritesQuerySchema,
+  recipeIdParamsSchema,
+} from "../schemas/recipesSchemas.js";
 
 const recipesRouter = express.Router();
 
 // Применяем auth middleware ко всем роутам
 recipesRouter.use(auth);
 
-recipesRouter.get('/own', ctrlWrapper(recipesController.getOwnRecipes));
+recipesRouter.get("/own", ctrlWrapper(recipesController.getOwnRecipes));
 
 // Видалити власний рецепт
-recipesRouter.delete('/:id', ctrlWrapper(recipesController.deleteOwnRecipe));
+recipesRouter.delete("/:id", ctrlWrapper(recipesController.deleteOwnRecipe));
 
 // Додати рецепт до списку улюблених
-recipesRouter.post('/:id/favorite', ctrlWrapper(recipesController.addToFavorites));
+recipesRouter.post(
+  "/:id/favorite",
+  ctrlWrapper(recipesController.addToFavorites)
+);
 
 // GET /api/recipes/favorites - получить любимые рецепты пользователя
-recipesRouter.get('/favorites', validateQuery(getFavoritesQuerySchema), recipesController.getFavoriteRecipes);
+recipesRouter.get(
+  "/favorites",
+  validateQuery(getFavoritesQuerySchema),
+  ctrlWrapper(recipesController.getFavoriteRecipes)
+);
 
 // DELETE /api/recipes/favorites/:recipeId - удалить рецепт из избранного
 recipesRouter.delete(
-    '/favorites/:recipeId',
-    validateParams(recipeIdParamsSchema),
-    recipesController.removeFavoriteRecipe
+  "/favorites/:recipeId",
+  validateParams(recipeIdParamsSchema),
+  ctrlWrapper(recipesController.removeFavoriteRecipe)
 );
 
 export default recipesRouter;
