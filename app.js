306a7372df062eb74e74e5630bcd87438b1d5279
serhiizghama <zmrser@gmail.com>
2025-07-10 16:55:51 +0700

    Update environment configuration, enhance user seeding with password hashing, and adjust API routes

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.json());
 
 // app.use("/api/contacts", contactsRouter);
 // app.use("/api/auth", authRouter);
-app.use('/users', userRouter);
+app.use('/api/users', userRouter);
 app.use('/api/recipes', recipesRouter);
 app.use('/api/ingredients', ingredientsRouter);
 
