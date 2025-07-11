5ed788a912a55b3eb4156aa6df9409ef3612989d
Vitalii Kostenko <apelsin@i.ua>
2025-07-11 21:51:16 +0200

    commit merge

--- config/config.js
+++ config/config.js
@@ -1,29 +1,27 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+const isSSL = process.env.DB_SSL === 'true';
+
 const common = {
     username: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'foodies',
     host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
     dialect: 'postgres',
     logging: false,
-    dialectOptions:
-        process.env.DB_SSL === 'true'
-            ? {
-                  ssl: {
-                      require: true,
-                      rejectUnauthorized: false,
-                  },
-              }
-            : {},
+    ...(isSSL && {
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    }),
 };
 
-
-
 console.log("🔍 DB_USER from .env:", process.env.DB_USER);
-
 
 export default {
     development: { ...common },
